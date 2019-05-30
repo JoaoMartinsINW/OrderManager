5816bd933cd639f962fbe1c2fd9bd6de86207b07
JoaoMartins <joao.martins.noventa@gmail.com>
2019-05-30 13:45:59 +0100

    reduce variable instanciation

--- src/main/java/com/sigma/ps/om/structure/transformer/Transformer.java
+++ src/main/java/com/sigma/ps/om/structure/transformer/Transformer.java
@@ -1,39 +1,24 @@
 package com.sigma.ps.om.structure.transformer;
 
-import java.util.List;
 import java.util.Map;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sigma.om.cmn.CommonUtils;
+import com.sigma.om.sdk.order.enrichment.change.OrderChange;
 import com.sigma.om.sdk.soi.framework.SoiContext;
 import com.sigma.om.sdk.soi.framework.SoiRequest;
 import com.sigma.om.soi.framework.SOIAdapterUtils;
 import com.sigma.om.soi.framework.SoiConfig;
-import com.sigma.om.soi.framework.exception.SoiRuntimeException;
 import com.sigma.om.soi.framework.exception.SoiTransformerException;
 import com.sigma.ps.om.commons.envcfg.EnvironmentUtil;
+import com.sigma.ps.om.commons.order.OrderUtils;
 import com.sigma.ps.om.commons.soi.SoiMapping;
 import com.sigma.ps.om.commons.soi.mapping.pojo.MappingConfig;
 
-
 public abstract class Transformer implements Processor {
-
-	final protected SoiContext soiContext;
-	final protected String adapterName;
-	final protected Map<String,String> envCfg;
-	final protected List<SoiRequest> soiRequestList;
-
-	Transformer(Exchange exchange) throws Exception{
-		this.soiContext = SoiConfig.getSoIContextFromInBody(exchange);
-		this.adapterName = SOIAdapterUtils.getSoiAdapter(exchange).getAdapterName();
-		this.envCfg = EnvironmentUtil.getEnvConfig(this.adapterName);
-		this.soiRequestList = this.soiContext.getSoiRequestList();
-	}
 
 	public MappingConfig  getSoiMappingConfig(String adapterName, String filepath) {
 		MappingConfig mappingConfig = new MappingConfig();
@@ -52,20 +37,35 @@ public abstract class Transformer implements Processor {
 
 	@Override
     public void process(Exchange exchange) throws Exception {
-		stepBefore();
-		buildPayload();
-		manualTask();
-		stepAfter();
+		final SoiContext soiContext = SoiConfig.getSoIContextFromInBody(exchange);
+		final String adapterName = SOIAdapterUtils.getSoiAdapter(exchange).getAdapterName();
+
+		exchange = setHeaders(exchange, EnvironmentUtil.getEnvConfig(adapterName));
+
+		for(SoiRequest soiRequest : soiContext.getSoiRequestList()) {
+			Object request = buildRequest(soiRequest);
+
+			if(request!= null) {
+				final OrderChange orderChange = soiRequest.getEnrichmentInfo().getIpOrderChange();
+				if (orderChange!= null) {
+					request = manualTask(soiRequest, orderChange);
+				}
+				final String payload = SoiMapping.getJsonBuilder(request);
+
+				if(payload!=null) {
+					setPayload(payload);
+					soiContext.setSoiExternalInteractions(OrderUtils.populateExternalInteractions(soiRequest));
+				}
+			}
+		}
 	}
 
-	public abstract void stepBefore();
+	public abstract Exchange setHeaders(Exchange exchange, Map<String,String> envCfg);
 
-	public abstract void buildPayload();
+	public abstract Object buildRequest(SoiRequest soiRequest);
 
-	public abstract void manualTask();
+	public abstract Object manualTask(SoiRequest soiRequest, OrderChange orderChange);
 
-	public void stepAfter() {
-
-	}
+	public abstract void setPayload(Object request);
 
 }
